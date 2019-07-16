e9401c8c97c676e2509ca83a84ca7b8ebdb4c83e
BridgetLincy <37259133+BridgetLincy@users.noreply.github.com>
2019-07-16 15:42:46 +0530

    Update set3.py

--- set3.py
+++ set3.py
@@ -3,7 +3,7 @@ if((a>='a' and a<='z')or(a>='A' and a<='Z')):
     
    if (a=='a'or a=='e'or a=='i'or a=='o'or a=='u'):
       print("Vowel")
-   if (a=='A'or a=='E'or a=='I'or a=='O'or a=='U'):
+   elif (a=='A'or a=='E'or a=='I'or a=='O'or a=='U'):
        print("Vowel")
    else:
       print("Consonant")
